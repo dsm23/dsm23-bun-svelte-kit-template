b18e8e9ee5d64c3f6ed4f8d89a3506e8e54890a3
David Murdoch <dsmurdoch357@gmail.com>
2025-08-05 18:30:45 +0100

    test(playwright): add basic tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<svelte:head>
+  <title>SvelteKit Enterprise Boilerplate</title>
+  <meta
+    name="description"
+    content="This is where the description goes for SEO"
+  />
+</svelte:head>
+
+<main>
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+  </p>
+</main>
